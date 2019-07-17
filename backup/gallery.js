bb4a588818f1454b860785c47592fea7d37f6813
Utkarsh Gupta <utkarshg99@gmail.com>
2019-07-17 14:17:18 +0530

    Site v1.0 ready

--- backup/gallery.js
+++ backup/gallery.js
@@ -1,3 +1,11 @@
+const config={
+    apiKey: "AIzaSyAK-Yx35id8igBFKqcPBkze06TkfZ5q89E",
+    authDomain: "lifi-iitk.firebaseapp.com",
+    databaseURL: "https://lifi-iitk.firebaseio.com",
+    projectId: "lifi-iitk",
+    appId: "1:516845378822:web:ea60b80adc89b12e"
+}
+firebase.initializeApp(config);
 var db = firebase.firestore();
 var hu = new Vue({
 el: '#portfolio',
--- deployment/public/js/gallery.js
+++ deployment/public/js/gallery.js
@@ -1,15 +1,16 @@
 var db = firebase.firestore();
 var hu = new Vue({
-el: '#gallery-vue',
+el: '#portfolio',
 data () {
     return {
     images: []
     }
 },
 mounted () {
-db.collection("gallery").orderBy("timeStamp", "desc").get().then((querySnapshot) => {
+    // .orderBy("timeStamp", "desc")
+    db.collection("gallery").get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
-            this.images.push(doc.data());
+        this.images.push(doc.data());
     });
     });
 }
